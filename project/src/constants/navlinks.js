72880ed9010436af7f000b649d2ac0f95cb78b6e
swarnim3103 <swarnim3103@gmail.com>
2024-10-08 12:17:45 +0530

    env set up: 	new file:   project/Router.jsx 	new file:   project/index.css 	modified:   project/index.html 	new file:   project/main.jsx 	modified:   project/package-lock.json 	modified:   project/package.json 	deleted:    project/src/App.css 	deleted:    project/src/App.jsx 	new file:   project/src/assets/icons/hamburger.svg 	new file:   project/src/constants/index.js 	new file:   project/src/constants/navlinks.js 	deleted:    project/src/index.css 	deleted:    project/src/main.jsx 	new file:   project/src/pages/App.jsx 	new file:   project/src/pages/Dashboard.jsx 	new file:   project/src/pages/index.js 	new file:   project/src/sections/About.jsx 	new file:   project/src/sections/Footer.jsx 	new file:   project/src/sections/Hero.jsx 	new file:   project/src/sections/Nav.jsx 	new file:   project/src/sections/index.js 	modified:   project/tailwind.config.js 	modified:   project/vite.config.js

---
+++ project/src/constants/navlinks.js
@@ -0,0 +1,34 @@
+const navlinks = [
+    {
+      pageName: "home",
+      links: [
+        {
+          title: "Home",
+          link: "#home",
+        },
+        {
+          title: "About",
+          link: "#about-us",
+        },
+        {
+          title: "Contact",
+          link: "#contact-us",
+        },
+       
+      ],
+    },
+    {
+      pageName: "services",
+      links: [
+        {
+          title: "Home",
+          link: "/",
+        },
+        {
+          title: "Library",
+          link: ".src/sections/Library.jsx",
+        },
+      ],
+    }
+  ];
+  export default navlinks;
---
+++ project/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {Nav, Footer} from '../sections/index.js';
+import { navlinks } from '../constants/index.js';
+const Dashboard = () => {
+  const scrollToSection = () => {
+    const section = document.getElementById('scroll-target');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+  return (
+    <>
+      <Nav page="services"/>
+      
+      
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
